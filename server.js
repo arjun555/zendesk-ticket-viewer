46f08755644809aad635e72f98801420c7e46f26
Arjun <arjunpatel555@gmail.com>
2019-06-09 13:44:20 +1000

    seperated axios requests out of server.js and into tickets.js

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
+const http = require ('./tickets')
 const express = require('express')
-const axios = require('axios')
+// const axios = require('axios')
 const dotenv = require ('dotenv')
 const app = express()
 const port = 8080;
-
 // configure dotenv module
 dotenv.config()
-
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
 
-app.get('/', (req, res) => {
-    res.send('hi')
+app.get('/', (req, appRes) => {
+
+    http.getAllTicketData().then((result) => console.log(result))
+                            .catch((error) => console.log(error))
+
 })
 
-const token = new Buffer(`${process.env.ZENDESK_API_USER}:${process.env.ZENDESK_API_PASS}`).toString('base64')
-axios.get(`https://${process.env.ZENDESK_API_DOMAIN}.zendesk.com/api/v2/tickets?per_page=3`, {
- headers: {
-   Authorization: `Basic ${token}` 
- }
-}).then(res => console.log(res))
-    .catch(res => console.log(res.response))
+
---
+++ tickets.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+const dotenv = require ('dotenv')
+dotenv.config()
+
+const token = new Buffer(`${process.env.ZENDESK_API_USER}:${process.env.ZENDESK_API_PASS}`).toString('base64')
+// Get tickets from API
+function getAllTicketData (){
+    return axios.get(`https://${process.env.ZENDESK_API_DOMAIN}.zendesk.com/api/v2/tickets?per_page=3`, {
+        headers: {
+            Authorization: `Basic ${token}` 
+        }})
+        .then(res => {
+            return (res.data)})
+        .catch(res => {
+            return (res.response)})
+}
+
+module.exports = {
+    getAllTicketData: getAllTicketData
+}
