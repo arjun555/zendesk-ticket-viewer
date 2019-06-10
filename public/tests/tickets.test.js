b75815509a1c350687237e16c8f10875ecd29366
Arjun <arjunpatel555@gmail.com>
2019-06-10 13:47:48 +1000

    tested setToken and getTickets return their expected datat types

--- public/tests/tickets.test.js
+++ public/tests/tickets.test.js
@@ -1,5 +1,24 @@
 const {Tickets} = require('../../tickets')
 
-test('Tickets.token returns a string', () =>{
-    expect(typeof new Tickets().token).toBe('string')
+
+describe('Tickets class', ()=>{
+
+    let testTickets = new Tickets();
+
+    test('Tickets.token returns a string', () =>{
+        expect(typeof testTickets.token).toBe('string')
+    })
+    
+    test('setToken returns a string', () =>{
+        expect(typeof testTickets.setToken('')).toBe('string')
+    })
+
+    test('getTickets() returns an object', () =>{
+        expect(typeof testTickets.getTickets()).toBe('object')
+    })
+
+    test('setNextPage() will take the data returned from the api call, and set the nextPage property', () =>{
+        expect(testTickets.setNextPage())
+    })
+
 })
--- tickets.js
+++ tickets.js
@@ -3,9 +3,40 @@ const dotenv = require ('dotenv')
 
 class Tickets {
 
-    constructor(){
-        this.token = ''
+    constructor(user, domain){
+        this.user = user,
+        this.domain = domain,
+        this.token = '',
+        this.nextPage = null,
+        this.previousPage = null,
+        this.count = null,
+        this.perPage = 25
     }
+
+    // Calculate and Set Token for API
+    setToken(password){
+        this.token = new Buffer(`${this.user}:${password}`).toString('base64')
+        return this.token
+    }
+
+    setNextPage(data){
+        
+    }
+
+    // Get tickets from API. Number of tickets data is capped by the 'numberOfTickets' arg
+    getTickets (numberOfTickets=25){
+    return axios.get(`https://${this.domain}.zendesk.com/api/v2/tickets?per_page=${numberOfTickets}`, {
+        headers: {
+            Authorization: `Basic ${this.token}` 
+        }})
+        .then(res => {
+            // status will be 200
+            return (res.data)})
+        .catch(res => {
+            // status will be  >= 400
+            return (res.response)})
+    }
+
 }
 
 module.exports = {Tickets}
